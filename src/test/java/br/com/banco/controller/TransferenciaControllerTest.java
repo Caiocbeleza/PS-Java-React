a823d94deb4af04807b018aa4cea36e19caf328e
Caio César Beleza <caio.almeidabeleza@gmail.com>
2023-07-18 17:50:13 -0300

    Implementing first test

--- src/test/java/br/com/banco/controller/TransferenciaControllerTest.java
+++ src/test/java/br/com/banco/controller/TransferenciaControllerTest.java
@@ -4,6 +4,8 @@ package br.com.banco.controller;
 import br.com.banco.DTO.TransferenciaDTO;
 import br.com.banco.entity.Transferencia;
 import br.com.banco.repository.TransferenciaRepository;
+import br.com.banco.service.TransferenciaService;
+import br.com.banco.service.TransferenciaServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +29,14 @@ public class TransferenciaControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    TransferenciaRepository repository;
+    TransferenciaServiceImpl service;
+
 
     @Test
     void buscarTransacoes() throws Exception{
         var transferencia = new TransferenciaDTO();
         transferencia.setNomeOperador("Fulano");
-        Mockito.when(repository.pegarTodosRegistros()).thenReturn(List.of(transferencia));
+        Mockito.when(service.findAll()).thenReturn(List.of(transferencia));
         this.mockMvc.perform(get("/transferencias"))
                 .andDo(print())
                 .andExpect(status().isOk())
